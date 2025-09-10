dc106740cb97d5b4ec173a2b8bb7f84ba3e6b050
loulou123546 <louis.maillard@protonmail.com>
2025-09-10 07:48:00 +0200

    feat: improved editor reactivity

--- src/components/InkPlayer.svelte
+++ src/components/InkPlayer.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-import { Compiler } from "inkjs/full";
+import { Compiler, type Story } from "inkjs/full";
+import { untrack } from "svelte";
 
 interface Props {
 	value?: string;
@@ -8,12 +9,27 @@ interface Props {
 
 const { value = "", class: className = "" }: Props = $props();
 
-const story = $derived(new Compiler(value).Compile());
-
 let texts: string[] = $state([]);
 let choices: { text: string; index: number }[] = $state([]);
 
+const story: Story | { message: string; type: number }[] = $derived.by(() => {
+	const errors: { message: string; type: number }[] = [];
+	try {
+		return new Compiler(value, {
+			errorHandler: (message: string, type: number) => {
+				errors.push({ message, type });
+			},
+		}).Compile();
+	} catch (err) {
+		if (errors.length < 1) {
+			errors.push({ message: `${err?.name}: ${err?.message}`, type: -1 });
+		}
+		return errors;
+	}
+});
+
 function playScript() {
+	if (!story || Array.isArray(story)) return;
 	while (story.canContinue) {
 		texts.push(story.Continue() ?? "--- no more text ---");
 	}
@@ -21,31 +37,47 @@ function playScript() {
 }
 
 function playChoice(index: number) {
+	if (!story || Array.isArray(story)) return;
 	story.ChooseChoiceIndex(index);
 	choices = [];
 	playScript();
 }
-playScript();
 
 function reset() {
 	texts = [];
 	choices = [];
+	if (!story || Array.isArray(story)) return;
 	story.ResetState();
 	playScript();
 }
+
+$effect(() => {
+	if (value)
+		// track ink text change
+		untrack(() => reset()); // but don't track all variables modified by reset
+});
 </script>
 
 <div class={`flex flex-col ${className}`}>
     <div class="w-full bg-gray-100 flex flex-row p-2">
         <button onclick={reset} class="bg-gray-300 hover:bg-gray-400 py-2 px-4 rounded-lg">Reset</button>
+		{#if Array.isArray(story)}
+			<p class="py-2 px-4">There are {story.length} errors / warnings</p>
+		{/if}
     </div>
 
     <div class="p-4 w-full">
-        {#each texts as text}
-            <p>{text}</p>
-        {/each}
-        {#each choices as choice}
-            <button class="font-semibold text-gray-700 hover:text-gray-950 block" onclick={() => playChoice(choice.index)}>{choice.text}</button>
-        {/each}
+		{#if Array.isArray(story)}
+			{#each story as error}
+				<p class="py-2 px-4">{error.message}</p>
+			{/each}
+		{:else}
+			{#each texts as text}
+				<p>{text}</p>
+			{/each}
+			{#each choices as choice}
+				<button class="font-semibold text-gray-700 hover:text-gray-950 block" onclick={() => playChoice(choice.index)}>{choice.text}</button>
+			{/each}
+		{/if}
     </div>
 </div>
--- src/pages/admin/editor/_main.svelte
+++ src/pages/admin/editor/_main.svelte
@@ -3,8 +3,9 @@ import { faro } from "@grafana/faro-web-sdk";
 import type { Stories, Story } from "@shared/types/Story";
 import debounce from "lodash/debounce";
 import InkEditor from "../../../components/InkEditor.svelte";
-import InkGraph from "../../../components/InkGraph.svelte";
+// import InkGraph from "../../../components/InkGraph.svelte";
 import InkPlayer from "../../../components/InkPlayer.svelte";
+import LoginWall from "../../../components/LoginWall.svelte";
 import {
 	createStory,
 	editStory,
@@ -12,12 +13,17 @@ import {
 	listStoriesByAuthor,
 } from "../../../models/stories";
 
+let okLogin: boolean = $state(false);
 let loading: boolean = $state(true);
 let saving: number = $state(Date.now());
 let latest_save: number = $state(Date.now());
 let stories: Stories = $state([]);
 let selected_story: Story | undefined = $state(undefined);
 let inkContent: string = $state("");
+
+function loginDone() {
+	okLogin = true;
+}
 
 listStoriesByAuthor("me")
 	.then((data) => {
@@ -59,7 +65,7 @@ const saveStory = debounce(
 	() => {
 		const saved_at = Date.now();
 		if (!selected_story) return;
-		editStory(selected_story)
+		return editStory(selected_story)
 			.then(() => {
 				latest_save = saved_at;
 			})
@@ -76,39 +82,50 @@ function handleInkChange(value: string) {
 	selected_story.ink = value;
 	saveStory();
 }
+
+async function forceSaveBack() {
+	await saveStory.flush();
+	selected_story = undefined;
+}
 </script>
 
 <main class="p-8">
-<section class="p-6 border border-gray-400 rounded-xl">
-    <h2 class="text-xl text-center pb-4 font-semibold">Mes histoires</h2>
-    <div class="flex flex-row flex-wrap gap-x-4 gap-y-2">
-        {#each stories as story}
-            <button onclick={() => selectStory(story.id)} class="bg-gray-200/85 hover:bg-gray-300 px-4 py-2 rounded-lg">{story.name}</button>
-        {/each}
-        <button onclick={newStory} class="bg-gray-200/85 hover:bg-gray-300 px-4 py-2 rounded-lg"><i class="fa fa-plus pr-1"></i> Nouvelle histoire</button>
-    </div>
-</section>
+	{#if !okLogin}
+        <LoginWall whenLoginOK={loginDone} />
+    {:else}
+		{#if !selected_story}
+			<section class="p-6 border border-gray-400 rounded-xl">
+				<h2 class="text-xl text-center pb-4 font-semibold">Mes histoires</h2>
+				<div class="flex flex-row flex-wrap gap-x-4 gap-y-2">
+					{#each stories as story}
+						<button onclick={() => selectStory(story.id)} class="bg-gray-200/85 hover:bg-gray-300 px-4 py-2 rounded-lg">{story.name}</button>
+					{/each}
+					<button onclick={newStory} class="bg-gray-200/85 hover:bg-gray-300 px-4 py-2 rounded-lg"><i class="fa fa-plus pr-1"></i> Nouvelle histoire</button>
+				</div>
+			</section>
 
-
-
-{#if selected_story}
-<section class="w-full p-6 border border-gray-400 rounded-xl">
-    <h3 class="text-lg font-semibold mb-4">{selected_story.name}
-		{#if saving <= latest_save}
-		<i class="fa-regular fa-circle-check text-green-600"></i>
 		{:else}
-		<i class="fa-solid fa-rotate text-gray-700 fa-spin"></i>
+			<button onclick={forceSaveBack} class="bg-gray-200/85 hover:bg-gray-300 px-4 py-2 mb-2 rounded-lg"><i class="fa fa-arrow-left pr-1"></i> Retour</button>
+			
+			<section class="w-full p-6 border border-gray-400 rounded-xl">
+				<h3 class="text-lg font-semibold mb-4">{selected_story.name}
+					{#if saving <= latest_save}
+					<i class="fa-regular fa-circle-check text-green-600"></i>
+					{:else}
+					<i class="fa-solid fa-rotate text-gray-700 fa-spin"></i>
+					{/if}
+				</h3>
+				<div class="w-full flex flex-row flex-wrap">
+					<InkEditor 
+						value={inkContent} 
+						onchange={handleInkChange} 
+						class="h-[50vh] w-1/2" 
+					/>
+					<InkPlayer value={inkContent} class="w-1/2" />
+				</div>
+					<!-- <InkGraph value={inkContent} class="w-full" /> -->
+			</section>
 		{/if}
-	</h3>
-	<div class="w-full flex flex-row flex-wrap">
-		<InkEditor 
-			value={inkContent} 
-			onchange={handleInkChange} 
-			class="h-[50vh] w-1/2" 
-		/>
-		<InkPlayer value={inkContent} class="w-1/2" />
-	</div>
-		<InkGraph value={inkContent} class="w-full" />
-</section>
-{/if}
+
+	{/if}
 </main>
